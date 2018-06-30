f1afe73f4b736d2648d5ba0b4167b2b03099e853
yumikomatsumoto <matsumoto.0124.dm@gmail.com>
2018-06-30 18:38:24 +0900

    Lesson7　課題

---
+++ Assets/CubeControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControll : MonoBehaviour {
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+
+    //消滅位置
+    private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine) {
+            Destroy(gameObject);
+        }
+
+		
+	}
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    //ゲームオーバーテキスト
+    private GameObject gameOverText;
+
+    //走行距離テキスト
+    private GameObject runLengthText;
+
+    //走った距離
+    private float len = 0;
+
+    //走る速度
+    private float speed = 0.03f;
+
+    //ゲームオーバーの判定
+    private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        //シーンビューからオブジェクトの実態を検索する
+        this.gameOverText = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            if(this.isGameOver == false) {
+                //  走った距離を更新する
+                this.len += this.speed;
+
+            //  走った距離を表示する
+            this.runLengthText.GetComponent<Text>().text = "Distance:   " + len.ToString("F2") + "m";
+        }
+
+        //ゲームオーバーになった場合
+        if (isGameOver) {
+            //クリックされたらシーンをロードする
+            if (Input.GetMouseButtonDown(0)) {
+                // GameSceneを読み込む
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+		
+	}
+
+    public void GameOver() {
+        //ゲームオーバーになった時、画面上にゲームオーバーを表示する
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
